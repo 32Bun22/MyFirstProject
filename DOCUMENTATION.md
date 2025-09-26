93f9dbca2f2539a1a0dad80a25c813132c52b07e
student1 <student1@class.local>
2025-09-26 10:13:35 +0900

    doc added

---
+++ DOCUMENTATION.md
@@ -0,0 +1,8 @@
+# Функция calculate_sum
+
+## Описание
+Возварщает сумму двух чисел
+## Использование
+```python
+result = calculate_sum(5,3)
+print(result) #8
--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-print("Моя первая программа")
-print("Огонерова и Яковлева")
+def sum(a, b, c):
+    result = a + b + c
+    return result
+x = 5
+y = 10
+z = 11
+total = sum(x, y, z)
+print("Сумма: ", total)
